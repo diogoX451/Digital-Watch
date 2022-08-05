148d890533e59bce93b853a6f610846039674a97
diogoX451 <diogo.g07@icloud.com>
2022-08-04 22:18:48 -0300

    Atualização
    https://node-postgres.com/features/queries
    
    Testar se passa push...

--- dataBase/dataBase.js
+++ dataBase/dataBase.js
@@ -1,23 +1,34 @@
-const { Pool } = require('pg');
-const pg = require('pg').Pool;
+const { Pool } = require("pg");
+const pg = require("pg").Pool;
+const router = require("express").Router();
+const app = require("../src/index.js");
 const client = new Pool({
-    user: 'thqkpwob',
-    host: 'kesavan.db.elephantsql.com',
-    database: 'thqkpwob',
-    password: '74oPAldu-IVYK1b1lFyGQhRia-NAkhPG',
-    port: 5432,
-})
-const user = ("/users", (req, res) => {
-    client.query('SELECT * FROM "public"."visitas" LIMIT 100', (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        res.status(200).json(result.rows);
-
-    });
+  user: "thqkpwob",
+  host: "kesavan.db.elephantsql.com",
+  database: "thqkpwob",
+  password: "74oPAldu-IVYK1b1lFyGQhRia-NAkhPG",
+  port: 5432,
 });
+const user = (req, res) => {
+  client.query('SELECT * FROM "public"."visitas" LIMIT 100', (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.status(200).json(result.rows);
+  });
+};
 
+const newUser = (req, res) => {
+  const text = "INSERT INTO visitas(id, nome) VALUES($1, $2) RETURNING *";
+  const values = [app.lista];
+  client.query(text, values, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
 module.exports = {
-    client,
-    user
-}
+  user,
+  client,
+  newUser,
+};
--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ app.set("views", "./model");
 app.use(express.static("./model"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const client = require('../dataBase/dataBase.js');
+const client = require("../dataBase/dataBase.js");
+const lista = [];
+app.get("/list", client.user);
 
-app.get("/digitalWatch", client.user);
-// app.get("/digitalwatch", (req, res) => {
-//   res.render("index");
-// });
+app.post("/newUser", client.newUser, (req, res) => {
+  const user = req.body.name;
+  lista.push(user);
+  res.redirect("/list");
+});
+
+app.get("/digitalwatch", (req, res) => {
+  res.render("index");
+});
+
+module.exports = {
+  lista,
+};
 
 app.listen(3000);
